6950cd93632042be12e0acf9ef33d6e29a4bdb9d
Alex Berman <alexberman999@gmail.com>
2019-11-04 21:27:46 -0500

    Added skeleton functions

--- rrt.py
+++ rrt.py
@@ -3,3 +3,17 @@ class rrtNode:
 		self.x = x
 		self.y = y
 		self.parent = parent
+
+DELTA = 20
+
+def rrt(startX, startY, destX, destY):
+
+#Determine if the child is in a valid location
+def valid(nodeChild):
+
+#Find the nearest node to the child node
+def nearestPoint(nodeRoot, nodeChild):
+
+#Take the nearest node and move the child to within the distance DELTA of it
+def steer(nodeChild):
+
